35557b48eeeb374c724abaf98540c254601e5294
Nicolas NOSAL <nicolas.nosal@gmail.com>
2025-08-12 11:40:49 +0200

    init

---
+++ __main__.py
@@ -0,0 +1,30 @@
+import pulumi, os, textwrap, requests
+from src.provisionner.proxmox import ProxmoxProvider as Proxmox
+#from src.tools.file import LoadFile
+
+if __name__ == "__main__":
+
+  # Get Config + Connect to Proxmox...
+  config = pulumi.Config('pulumox')
+  proxmoxConfig = {
+      "endpoint": config.require("pve_url"),
+      "insecure": config.get_bool("pve_url_insecure") or False,
+      "username": config.require("pve_username"),
+      "password": config.require_secret("pve_password")
+    }
+  cluster = Proxmox(  name='default', config=proxmoxConfig )
+
+  # ###############
+  # ### WINDOWS ###
+  # ###############
+  #setupWindows(cluster)
+
+  # #############
+  # ### MACOS ###
+  # #############
+  #setupMacOS(cluster)
+
+  # #############
+  # ### LINUX ###
+  # #############
+  #setupLinux(cluster)
